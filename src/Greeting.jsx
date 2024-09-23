9ff84070db974cae532cb139647bab0ef17b9fc3
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-09-23 18:08:24 +0200

    feat: add Person component as react state management exercise

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,5 +1,66 @@
-function Greeting() {
-	return <h1>&quot;Greetings, weary traveler! If you got the coins, Khajiit has the wares.&quot;</h1>
+import { useState } from 'react';
+
+function List(props) {
+	if (!props.animals) {
+		return <div>Loading...</div>
+	}
+
+	if (props.animals.length === 0) {
+		return <div>There are no animals in the list!</div>
+	}
+
+	return (
+		<ul>
+			{props.animals.map((animal) => {
+				return <li key={animal}>{animal}</li>
+			})}
+		</ul>
+	)
 }
 
-export default Greeting;
+export function Greeting() {
+	const animals = ["Lion", "Cow", "Snake", "Lizard"];
+
+	return (
+		<div>
+			<h1>Animals: </h1>
+			<List animals={animals} />
+		</div>
+	)
+}
+
+export function Person() {
+  const [person, setPerson] = useState({ firstName: "First", lastName: "Last", age: 100 });
+
+  const handleIncreaseAge = () => {
+    setPerson({ ...person, age: person.age + 1 });
+  };
+
+	const changeFirstName = (val) => {
+    setPerson({ ...person, firstName: val });
+  };
+
+	const changeLastName = (val) => {
+    setPerson({ ...person, lastName: val });
+  };
+
+  return (
+    <>
+      <h1>{`${person.firstName} ${person.lastName}`}</h1>
+      <h2>{person.age}</h2>
+      <button onClick={handleIncreaseAge}>Increase age</button>
+			<h3>First name</h3>
+			<input
+				type="text"
+				value={person.firstName}
+				onChange={(event) => changeFirstName(event.target.value)}
+			/>
+			<h3>Last name</h3>
+			<input
+				type="text"
+				value={person.lastName}
+				onChange={(event) => changeLastName(event.target.value)}
+			/>
+    </>
+  );
+}
